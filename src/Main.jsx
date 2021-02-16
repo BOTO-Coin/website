19aed400d9300f8744460822a72baf6a8465ec0e
Pedro Laxe <pedrolaxe@gmail.com>
2021-02-16 20:41:25 -0300

    Add Instagram Button

--- src/Main.jsx
+++ src/Main.jsx
@@ -10,7 +10,8 @@ export default function Main() {
         <div className="title">
           <img className="logo" src={boto} alt="boto" />
           <h1>BOTO Coin</h1>
-          <p>The Brazilian Token designed to save the Amazonia</p>
+          <br />
+          <p>The Brazilian token designed to raise funds to help the Amazonia</p>
           <br />
           <a className="mr-5" href="https://t.me/BOTOCoin" target="_blank">
             <svg
@@ -51,6 +52,28 @@ export default function Main() {
               <path d="M22 4.01c-1 .49-1.98.689-3 .99-1.121-1.265-2.783-1.335-4.38-.737S11.977 6.323 12 8v1c-3.245.083-6.135-1.395-8-4 0 0-4.182 7.433 4 11-1.872 1.247-3.739 2.088-6 2 3.308 1.803 6.913 2.423 10.034 1.517 3.58-1.04 6.522-3.723 7.651-7.742a13.84 13.84 0 0 0 .497 -3.753C20.18 7.773 21.692 5.25 22 4.009z"></path>
             </svg>
           </a>
+          <a
+            href="https://www.instagram.com/boto.digital"
+            target="_blank"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-brand-instagram"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="#9e9e9e"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <rect x="4" y="4" width="16" height="16" rx="4" />
+              <circle cx="12" cy="12" r="3" />
+              <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+            </svg>
+          </a>
         </div>
 
         <div className="section">
@@ -68,7 +91,6 @@ export default function Main() {
             <ul>
               <li>1% burn every swap</li>
               <li>1% distributed to holders every swap</li>
-              <li>Use at least 2.5% slippage when swap</li>
             </ul>
           </div>
           <div>
